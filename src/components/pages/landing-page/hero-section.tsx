d676136045c6c731b456cdf0a42be943689aef4e
kkopoku <kk.opoku@outlook.com>
2024-12-31 21:15:05 +0000

    Add: hero section to landing page

---
+++ src/components/pages/landing-page/hero-section.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+export function HeroSection() {
+  return (
+    <motion.div
+      className="text-center space-y-2 my-5"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h1 className="text-3xl sm:text-5xl font-bold tracking-tight text-gray-900">
+        Securely Share Data &nbsp;<span className="block text-purple-600">Effortlessly</span>
+      </h1>
+      <p className="mx-auto max-w-2xl text-xs text-gray-600">Send self-destructing messages with military-grade encryption—ideal for sharing sensitive information securely.</p>
+    </motion.div>
+  )
+}
+
