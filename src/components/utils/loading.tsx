27df9537b786cd1ead608cfba87a7f016dd00279
kkopoku <kk.opoku@outlook.com>
2024-12-31 21:13:43 +0000

    chore: added loading component

---
+++ src/components/utils/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading(){
+    return (
+        <div className="rounded-full justify-center animate-spin h-40 w-40 border-[15px] border-b-green-600 border-gray-300"></div>
+    )
+}
