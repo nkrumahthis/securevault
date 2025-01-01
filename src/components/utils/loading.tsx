19d01267bdd764d8883617e47190453da114c97d
kkopoku <kk.opoku@outlook.com>
2025-01-01 05:17:39 +0000

    chore: updated loading

--- src/components/utils/loading.tsx
+++ src/components/utils/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading(){
     return (
-        <div className="rounded-full justify-center animate-spin h-40 w-40 border-[15px] border-b-green-600 border-gray-300"></div>
+        <div className="rounded-full justify-center animate-spin h-24 w-24 border-[10px] border-b-purple-600 border-gray-300"></div>
     )
 }
