e2c02b84744a1ced06fbd57899327c878586b535
kkopoku <kk.opoku@outlook.com>
2025-01-02 01:19:42 +0000

    chore: fix top bg issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#f9f3ff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
