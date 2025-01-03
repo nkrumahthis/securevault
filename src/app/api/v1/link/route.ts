743a41c58e260f1746ca6b20dade01e4a0158cd1
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-01-03 21:00:24 +0000

    create api routes

---
+++ src/app/api/v1/link/route.ts
@@ -0,0 +1,132 @@
+import Link from "@/models/link.model";
+import { generateRandomString } from "@/lib/utils";
+import sendEmail from "@/lib/email";
+import { runTest, encryptWithBaseKey, encryptWithUserPassphrase, decryptWithBaseKey, decryptWithUserPassphrase } from "@/lib/encryption";
+
+export const POST = async (req: Request, res: Response) => {
+    const body = await req.json();
+    const { message, viewNumber, lifetime, passphrase, recipient } = body;
+    const baseURL = process.env.SECUREVAULT_WEB;
+    const rand = generateRandomString(4);
+
+    // Validate inputs
+    if (!message || !viewNumber || !lifetime) {
+        return new Response(
+            JSON.stringify({ message: "Please fill in all required fields" }),
+            { status: 400 }
+        );
+    }
+
+    let encryptedMessage = "";
+    if (passphrase) {
+        encryptedMessage = encryptWithUserPassphrase(message, passphrase);
+    } else {
+        encryptedMessage = encryptWithBaseKey(message);
+    }
+
+    try {
+        const link = await Link.create({
+            message: encryptedMessage,
+            viewNumber: viewNumber,
+            lifetime: lifetime,
+            my_id: rand,
+            link: `${baseURL}${rand}`,
+            passphrase: passphrase ?? null,
+        });
+
+        if (recipient) {
+            console.log("It is getting here!");
+            const emailMessage = `SECUREVAULT ALERTS\nA secure message has been sent to you. Kindly click the link below to access your secure message:\nLink: ${link.link}\nPassword: ${passphrase ? passphrase : "n/a"}`;
+            try {
+                await sendEmail(recipient, "SecureVault Alert", emailMessage);
+                console.log("Email sent successfully");
+            } catch (emailError) {
+                console.error("Failed to send email:", emailError);
+            }
+        }
+
+        return new Response(
+            JSON.stringify({ message: "Link created successfully", link }),
+            { status: 201 }
+        );
+    } catch (e) {
+        console.log((e as Error).message);
+        return new Response(
+            JSON.stringify({ message: "Something went wrong while processing the request" }),
+            { status: 500 }
+        );
+    }
+};
+
+export const GET = async (req: Request, res: Response) => {
+    try {
+        const url = new URL(req.url);
+        const id = url.searchParams.get('id');
+        const passphrase = url.searchParams.get('passphrase');
+
+        if (!id) {
+            return new Response(
+                JSON.stringify({ message: "Please provide an id" }),
+                { status: 400 }
+            );
+        }
+
+        const foundLink = await Link.findOne({ my_id: id });
+
+        if (!foundLink || foundLink.expires_at < Date.now()) {
+            if (foundLink) await foundLink.deleteOne();
+            return new Response(
+                JSON.stringify({ message: "Link not found" }),
+                { status: 404 }
+            );
+        }
+
+        if (foundLink.passphrase) {
+            if (!passphrase) {
+                return new Response(
+                    JSON.stringify({ message: "Please provide a passphrase" }),
+                    { status: 401 }
+                );
+            } else {
+                const isMatch = await foundLink.comparePassphrase(passphrase);
+                if (!isMatch) {
+                    return new Response(
+                        JSON.stringify({ message: "Incorrect passphrase" }),
+                        { status: 401 }
+                    );
+                }
+            }
+        }
+
+        if (foundLink.viewNumber >= 2) {
+            foundLink.viewNumber--;
+            await foundLink.save();
+        } else {
+            await foundLink.deleteOne();
+        }
+
+        let decryptedMessage = foundLink.passphrase ?
+            decryptWithUserPassphrase(foundLink.message, passphrase!)
+            : decryptWithBaseKey(foundLink.message);
+
+        return new Response(
+            JSON.stringify({ message: decryptedMessage }),
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error in getLinkDetails:", error);
+        return new Response(
+            JSON.stringify({ message: "An error occurred while fetching link details" }),
+            { status: 500 }
+        );
+    }
+};
+
+const testing = async (req: Request, res: Response) => {
+    console.log("It is getting here!");
+    let result = runTest();
+    return new Response(
+        JSON.stringify({ message: "Testing route", result }),
+        { status: 200 }
+    );
+};
