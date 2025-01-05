2f859c2c83de27cbba47315ca6ec50e3ee2b84eb
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-01-05 18:22:58 +0000

    use cached connections if they exist

--- src/lib/connect.ts
+++ src/lib/connect.ts
@@ -1,5 +1,32 @@
 import mongoose from 'mongoose'
 
-export const connectDB = (url: string) => {
-    mongoose.connect(url)
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+    throw new Error("No MONGO_URI")
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null}
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = { bufferCommands: false }
+
+        cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+            return mongoose
+        })
+
+        cached.conn = await cached.promise;
+        return cached.conn;
+    }
+}
+
+export default dbConnect
